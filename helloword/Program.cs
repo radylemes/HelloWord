2a550a32555f32d30660fb00a2e1360fa814dc78
Daniel Lemes <danylemes@yahoo.com.br>
2021-11-06 09:53:05 -0300

    Primeira aula Git C#

---
+++ helloword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFilme, sala, hora, data;
+
+            Console.WriteLine("Digite o nome do Filme: ");
+            nomeFilme = Console.ReadLine();
+
+            Console.WriteLine("Digite a sala: ");
+            sala = Console.ReadLine();
+
+            Console.WriteLine("Qual a hora do Filme: ");
+            hora = Console.ReadLine();
+
+            Console.WriteLine("Data: ");
+            data = Console.ReadLine();
+
+            Console.WriteLine($"Seu ingresso para o filme {nomeFilme}, sala {sala} no dia {data} às {hora}");
+        }
+    }
+}
